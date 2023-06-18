b000fe90fb5b4dc882a6fc6f7917f447da6655b0
splitcentre <raihanalfianpratama@gmail.com>
2023-06-18 20:02:27 +0700

    commit

--- cart.php
+++ cart.php
@@ -1,4 +1,30 @@
 <?php
+include 'database.php';
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+if(isset($_POST['update_cart'])){
+   $cart_id = $_POST['cart_id'];
+   $cart_quantity = $_POST['cart_quantity'];
+   mysqli_query($conn, "UPDATE `checkout` SET quantity = '$cart_quantity' WHERE id = '$cart_id'") or die('query failed');
+   $message[] = 'cart quantity updated!';
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   mysqli_query($conn, "DELETE FROM `checkout` WHERE id = '$delete_id'") or die('query failed');
+   header('location:cart.php');
+}
+
+if(isset($_GET['delete_all'])){
+   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+   header('location:cart.php');
+}
 
 ?>
 
@@ -18,35 +44,30 @@
     <title>main menu</title>
 </head>
 <body>
-    <div class="container mt-4" id="top-logo">
+<div class="container mt-4" id="top-logo">
         <div class="row">
             <div class="col-md-2" style="right: 5%;">
     <nav class="navbar navbar-light">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="index.php">
           <img src="image/insignia.png"  width="30" height="30" alt="">
           
         </a>
       </nav>
             </div>
             <div class="col-md-7" id="searchbar">
-                <div class="input-group mt-3">
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
-                    </div>
-                  </div>
             </div>
             <div class="col-md-3 mt-2" id="icons">
-              <div style="cursor: pointer;" class="circle float-right" onclick="window.location.href = 'login.html';" data-toggle="tooltip" data-placement="top" title="LOGIN & SIGNUP">
+              <div style="cursor: pointer;" class="circle float-right" onclick="redirectToProfile();" data-toggle="tooltip" data-placement="top" title="LOGIN & SIGNUP">
                 <i class="fas fa-user text-primary"></i>
             </div>
               
-             <a href="cart.html"> <div class="circle float-right mr-2"><i class="fas fa-shopping-cart text-primary"></i><sup><span class="badge badge-danger" style="width: 23px;">0</span></sup></div>
+             <a href="cart.php"> <div class="circle float-right mr-2"><i class="fas fa-shopping-cart text-primary"></i><sup><span class="badge badge-danger" style="width: 23px;">0</span></sup></div>
              </a>
             </div>
         </div>
     </div>
     <hr>
+
     <div class="container" style="height: 30px;">
       <nav class="navbar navbar-expand-lg navbar-light" style="height: 30px;">
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -55,27 +76,14 @@
           </button>
           <div class="collapse navbar-collapse pl-4" id="navbarNav">
               <ul class="navbar-nav">
-                  <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown"
-                          role="button" aria-haspopup="true" aria-expanded="false">
-                          <span>≡ All Category</span>
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="#">Transformer</a>
-                          <a class="dropdown-item" href="#">Anime</a>
-                          <div class="dropdown-divider"></div>
-                          <a class="dropdown-item" href="#">Evangelion</a>
-                          <a class="dropdown-item" href="#">Gundam</a>
-                      </div>
+                  <li class="nav-item active">
+                      <a class="nav-link" href="bandai.php">Bandai <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item active">
-                      <a class="nav-link" href="bandai.html">Bandai <span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="hasbro.php">Hasbro</a>
                   </li>
                   <li class="nav-item active">
-                      <a class="nav-link" href="hasbro.html">Hasbro</a>
-                  </li>
-                  <li class="nav-item active">
-                      <a class="nav-link" href="other.html">Other</a>
+                      <a class="nav-link" href="other.php">Other</a>
                   </li>
               </ul>
           </div>
@@ -83,109 +91,46 @@
   </div>
     <hr>
 
-    <div class="container" id="card">
-    <div class="row">
-        <div class="col-md-8">
-        <div class="card p-4" id="cards">
-            <h5 class="text-dark">Cart (2 Items)</h5>
-
-            <div class="row mt-4">
-                <div class="col-md-3">
-                  <div class="overlay"> 
-                    <img src="image/eva13a.jpg" class="zoom-in figure-img img-fluid">
-                   </div>
-
-                </div>
-
-                <div class="col-md-7">
-                    <h5 class="text-muted pt-2">Evangelion EVA UNIT 13</h5>
-                    <p class="text-uppercase font-weight-light">Model Kit</p>
-                    <button class="btn btn-sm text-muted text-uppercase"><i class="fas fa-trash-alt pr-2" onclick="removeItem(this)"></i>REMOVE ITEM</button>
-                    &nbsp;
-                    <button class="btn btn-sm text-muted text-uppercase"><i class="fas fa-heart pr-2"></i>ADD TO WISHLIST</button>
-
-                </div>
-
-                <div class="col-md-2">
-                    <div class="input-group input-group-sm">
-                        <div class="input-group-prepend" onclick="add2()">
-                          <span class="input-group-text"><i class="fas fa-plus"></i></span>
-                        </div>
-            
-                        <input type="text" class="numberbox2 form-control" value="1">
-                        <div class="input-group-append" onclick="subtract2()">
-                            <span class="input-group-text"><i class="fas fa-minus"></i></span>
-                        </div>
-                      </div>
-                </div>
-            </div>
-            <hr>
-            <div class="row">
-              <div class="col-md-3">
-                <div class="overlay"> 
-                  <img src="image/gundamjustice.jpg" class="zoom-in figure-img img-fluid">
-                 </div>
-
-              </div>
-
-              <div class="col-md-7">
-                  <h5 class="text-muted pt-2">Gundam Metal Build Justice Gundam</h5>
-                  <p class="text-uppercase font-weight-light">Model Kit</p>
-                  <button class="btn btn-sm text-muted text-uppercase" onclick="removeItem(this)"><i class="fas fa-trash-alt pr-2"></i>REMOVE ITEM</button>
-                  &nbsp;
-                  <button class="btn btn-sm text-muted text-uppercase"><i class="fas fa-heart pr-2"></i>ADD TO WISHLIST</button>
-
-              </div>
-
-              <div class="col-md-2">
-                  <div class="input-group input-group-sm">
-                      <div class="input-group-prepend" onclick="add3()">
-                        <span class="input-group-text"><i class="fas fa-plus"></i></span>
-                      </div>
-          
-                      <input type="text" class="numberbox3 form-control" value="1">
-                      <div class="input-group-append" onclick="subtract3()">
-                          <span class="input-group-text"><i class="fas fa-minus"></i></span>
-                      </div>
-                    </div>
-              </div>
-          </div>
-        </div>
-        </div>
-
-        <div class="col-md-4">
-        <div class="card p-3" id="card-cost">
-          <h5 class="text-primary pl-2">Total Amount</h5>
-          <table class="table table-borderless pt-2">
-           
-            <tbody>
-              <tr>
-                <th scope="row" class="font-weight-light">Product Amount</th>
-                <td>$179.00</td>
-              </tr>
-              <tr>
-                <th scope="row" class="font-weight-light">Shipping</th>
-                <td>$100.00</td>
-              </tr>
-            </tbody>
-          </table>
-          <hr>
-          <h6 class="pl-2 pt-1">Total Price <span class="float-right">$279.00</span></h6>
-          <hr>
-          <button class="btn btn-primary mt-2"><a href="checkout.html" class="text-white"><i class="fas fa-shopping-bag pr-2"></i>Proceed to Checkout</a></button>
-        </div>
-        </div>
-
-    </div>
-    <div class="row mt-5 mb-5">
-      <div class="col-md-8">
-        <div class="card p-3">
-          <h5 class="text-dark">Expected shipping delivery</h5>
-          <p class="text-muted">Thu., 12.03. - Mon., 16.03.</p>
-        
-        </div>
+    <div class="box-container">
+      <?php
+         $grand_total = 0;
+         $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+         if(mysqli_num_rows($select_cart) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($select_cart)){   
+      ?>
+      <div class="box">
+         <a href="cart.php?delete=<?php echo $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a>
+         <img src="uploaded_img/<?php echo $fetch_cart['image']; ?>" alt="">
+         <div class="name"><?php echo $fetch_cart['name']; ?></div>
+         <div class="price">$<?php echo $fetch_cart['price']; ?>/-</div>
+         <form action="" method="post">
+            <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
+            <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
+            <input type="submit" name="update_cart" value="update" class="option-btn">
+         </form>
+         <div class="sub-total"> sub total : <span>$<?php echo $sub_total = ($fetch_cart['quantity'] * $fetch_cart['price']); ?>/-</span> </div>
       </div>
-    </div>
+      <?php
+      $grand_total += $sub_total;
+         }
+      }else{
+         echo '<p class="empty">your cart is empty</p>';
+      }
+      ?>
+   </div>
+
+   <div style="margin-top: 2rem; text-align:center;">
+      <a href="cart.php?delete_all" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>" onclick="return confirm('delete all from cart?');">delete all</a>
+   </div>
+
+   <div class="cart-total">
+      <p>grand total : <span>$<?php echo $grand_total; ?>/-</span></p>
+      <div class="flex">
+         <a href="shop.php" class="option-btn">continue shopping</a>
+         <a href="checkout.php" class="btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">proceed to checkout</a>
+      </div>
+   </div>
+
 
     <div class="row">
       <div class="col-md-8">
@@ -200,4 +145,37 @@
         </div>
       </div>
     </div>
+
+    <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <h5>About Us</h5>
+          <p>A Toys Store specialized in action figures and model kit.</p>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <h5>Contact</h5>
+          <ul class="list-unstyled">
+            <li>Email: AlbertToys@yahoo.com</li>
+          </ul>
+        </div>
+        <div class="col-lg-4">
+          <h5>Follow Us</h5>
+          <ul class="list-inline">
+            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <p class="text-center">© 2023 AlbertToys. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+      
+
 </body>
